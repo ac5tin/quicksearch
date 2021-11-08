0427b40b0bca9aa1f01e04d85bf0b20b981b8fce
austin <austinchang4@gmail.com>
2021-11-08 16:36:37 +0800

    use redis to store list of postIDs and store all the scores in DB

--- indexer/store.go
+++ indexer/store.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"crypto/sha512"
 	"fmt"
-	"strconv"
 
 	gr "github.com/ac5tin/goredis"
 	"github.com/georgysavva/scany/pgxscan"
@@ -12,14 +11,20 @@ import (
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
+// Post DB table
 type Post struct {
-	ID        string             `db:"id" json:"id"`
-	Message   string             `db:"message" json:"message"`
-	Title     string             `db:"title" json:"title"`
-	URL       string             `db:"url" json:"url"`
-	Timestamp uint64             `db:"timestamp" json:"timestamp"`
-	Site      string             `db:"site" json:"site"`
-	Score     map[string]float32 `db:"score" json:"score"`
+	ID            string             `db:"id" json:"-"`
+	Author        string             `db:"author" json:"author"`
+	Site          string             `db:"site" json:"site"`
+	Title         string             `db:"title" json:"title"`
+	Tokens        map[string]float32 `db:"tokens" json:"tokens"`
+	Summary       string             `db:"summary" json:"summary"`
+	URL           string             `db:"url" json:"url"`
+	Timestamp     uint64             `db:"timestamp" json:"timestamp"`
+	Language      string             `db:"language" json:"language"`
+	InternalLinks []string           `db:"internal_links" json:"internal_links"`
+	ExternalLinks []string           `db:"external_links" json:"external_links"`
+	Entities      map[string]float32 `db:"entities" json:"entities"`
 }
 
 type Store struct {
@@ -34,57 +39,65 @@ func NewStore(rc *gr.Client, pg *pgxpool.Pool) Store {
 	}
 }
 
+// generate post id
 func (s *Store) genPostID(url string) string {
 	return fmt.Sprintf("%x", sha512.Sum512([]byte(url)))
 }
 
-// reverse index
-func (s *Store) getPostScoreListFromToken(token string, t *map[string]float32) error {
+// retrieve all post IDs of a given token
+func (s *Store) getPostIDListFromToken(token string, t *[]string) error {
 	rconn := (*s.rc).Get()
 	defer rconn.Close()
-	res, err := redis.StringMap((rconn.Do("HGETALL", token)))
+	res, err := redis.Strings((rconn.Do("LRANGE", token, 0, -1)))
 	if err != nil {
 		return err
 	}
-	for k, v := range res {
-		f, err := strconv.ParseFloat(v, 32)
-		if err != nil {
-			return err
-		}
-		(*t)[k] = float32(f)
-	}
+
+	*t = res
 	return nil
 }
 
-func (s *Store) addPostLink(token string, url string, score float32) error {
+// add a post to a given token
+func (s *Store) addPostLink(token string, url string) error {
 	postID := s.genPostID(url)
 	rconn := (*s.rc).Get()
 	defer rconn.Close()
 
-	if _, err := rconn.Do("HSET", token, postID, score); err != nil {
+	if _, err := rconn.Do("LPUSH", token, postID); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-// post data
-func (s *Store) getPostFromPostID(postID string, p *Post) error {
+// get full post data from a post ID
+func (s *Store) getPostFromPostIDs(postID *[]string, p *[]Post) error {
 	conn, err := s.pg.Acquire(context.Background())
 	if err != nil {
 		return err
 	}
 	defer conn.Release()
-	if err := pgxscan.Get(context.Background(), conn, p, `
-        SELECT id,message,title,url,timestamp,site,score
+
+	str := ""
+	for i, id := range *postID {
+		if i > 0 && i < len(*postID)-1 {
+			str += ","
+		}
+		str += fmt.Sprintf("'%s'", id)
+	}
+
+	if err := pgxscan.Select(context.Background(), conn, p, `
+        SELECT id,title,url,timestamp,site,author,language,summary,tokens,internal_links,external_links,entities
         FROM posts
-    `); err != nil {
+		WHERE id IN (%s)
+    `, str); err != nil {
 		return err
 	}
 
 	return nil
 }
 
+// insert (index) a post to store (redis and postgres)
 func (s *Store) InsertPost(p *Post) error {
 	conn, err := s.pg.Acquire(context.Background())
 	if err != nil {
@@ -98,9 +111,9 @@ func (s *Store) InsertPost(p *Post) error {
 	defer tx.Rollback(context.Background())
 	if _, err = tx.Exec(context.Background(), `
         INSERT INTO posts
-            (id,message,title,url,timestamp,site,score)
-            VALUES ($1,$2,$3,$4,$5,$6,$7)
-    `, p.ID, p.Message, p.Title, p.URL, p.Timestamp, p.Site, p.Score); err != nil {
+            (id,author,site,title,url,timestamp,language,summary,tokens,internal_links,external_links,entities)
+            VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)
+    `, s.genPostID(p.URL), p.Author, p.Site, p.Title, p.URL, p.Timestamp, p.Language, p.Summary, p.Tokens, p.InternalLinks, p.ExternalLinks, p.Entities); err != nil {
 		tx.Rollback(context.Background())
 		return err
 	}
