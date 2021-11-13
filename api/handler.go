1f487ebb9b43ed6fec562cb7a6e385a2e93e413a
austin <austinchang4@gmail.com>
2021-11-14 01:49:13 +0800

    api handler

--- api/handler.go
+++ api/handler.go
@@ -1,1 +1,15 @@
 package api
+
+import "github.com/gofiber/fiber/v2"
+
+func insertPost(c *fiber.Ctx) error {
+	return nil
+}
+
+func deletePost(c *fiber.Ctx) error {
+	return nil
+}
+
+func query(c *fiber.Ctx) error {
+	return nil
+}
