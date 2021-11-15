5ae5fd197bf1330a5b6e63f84d8bd7741f513e9d
austin <austinchang4@gmail.com>
2021-11-16 02:44:59 +0800

    test query

---
+++ indexer/query_test.go
@@ -0,0 +1,59 @@
+package indexer
+
+import (
+	"fmt"
+	"os"
+	"quicksearch/db"
+	"testing"
+
+	gr "github.com/ac5tin/goredis"
+	"github.com/joho/godotenv"
+)
+
+func TestQuery(t *testing.T) {
+	godotenv.Load("../.env")
+	// setup store
+	pg, err := db.Db(os.Getenv("DB_STRING"), os.Getenv("DB_SCHEMA"))
+	if err != nil {
+		t.Error(err)
+	}
+	rc := gr.NewRedisClient(fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT")), 0, "")
+	s := NewStore(rc, pg)
+	I = new(Indexer)
+	I.Store = &s
+
+	posts := new([]Post)
+	if err := I.QueryFullText("a.i data science", 10, 0, posts); err != nil {
+		t.Error(err)
+	}
+
+	if len(*posts) == 0 {
+		t.Error("Failed to query posts")
+	}
+
+	t.Logf("Number of results: %d", len(*posts))
+
+	for _, p := range *posts {
+		t.Logf("%s: %s", p.ID, p.Title)
+	}
+}
+
+func TestTokenQuery(t *testing.T) {
+	godotenv.Load("../.env")
+	// setup store
+	pg, err := db.Db(os.Getenv("DB_STRING"), os.Getenv("DB_SCHEMA"))
+	if err != nil {
+		t.Error(err)
+	}
+	rc := gr.NewRedisClient(fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT")), 0, "")
+	s := NewStore(rc, pg)
+	I = new(Indexer)
+	I.Store = &s
+
+	posts := new([]Post)
+	if err := I.QueryToken("ai", posts); err != nil {
+		t.Error(err)
+	}
+
+	t.Logf("Number of results: %d", len(*posts))
+}
