81032e049ee4d222fbd999ec88cbb085a81d7b49
austin <austinchang4@gmail.com>
2021-11-20 01:15:21 +0800

    tokenise multi

--- textprocessor/tokeniser.go
+++ textprocessor/tokeniser.go
@@ -50,3 +50,39 @@ func (tp *TextProcessor) Tokenise(input InputText, tokens *[]Token) error {
 	*tokens = (*res)[0]
 	return nil
 }
+
+func (tp *TextProcessor) TokeniseMulti(input *[]InputText, tokens *[][]Token) error {
+	inp := *input
+
+	b, err := json.Marshal(inp)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/api/tokenise", os.Getenv("TEXTPROCESSOR_ENDPOINT")), bytes.NewBuffer(b))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(resp.Body)
+		errMsg := fmt.Sprintf("status code not 200, Err: %s", buf.String())
+		return fmt.Errorf("textprocessor failed to tokenise text: %s", errMsg)
+	}
+
+	res := new([][]Token)
+	if err := json.NewDecoder(resp.Body).Decode(res); err != nil {
+		return err
+	}
+
+	*tokens = (*res)
+	return nil
+
+}
