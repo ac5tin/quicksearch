8316b716222ef2f463d7f440d774dd024b3e3a93
austin <austinchang4@gmail.com>
2021-12-06 12:38:31 +0800

    quickstore unit test

---
+++ indexer/quickstore/quickstore_test.go
@@ -0,0 +1,23 @@
+package quickstore
+
+import "testing"
+
+func TestQuickstore(t *testing.T) {
+	slaves := []string{}
+	mkey := "key"
+	qs := NewQuickStore(true, &slaves, &mkey)
+
+	key := "hello"
+	value := "world"
+	b := []byte(value)
+	qs.set(&key, &b)
+
+	r := new([]byte)
+	if err := qs.get(&key, r); err != nil {
+		t.Error("Error getting value")
+	}
+	if string(*r) != value {
+		t.Errorf("Expected %s, got %s", value, *r)
+	}
+
+}
